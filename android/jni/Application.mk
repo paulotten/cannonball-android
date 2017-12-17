31ce5fe61093a113b048fb61bbc21e8028212e37
Paul Otten <potten@my.bcit.ca>
2017-12-17 16:25:30 -0400

    Strip out everything but the Android SDL 1.2 library

--- android/jni/Application.mk
+++
@@ -1,9 +0,0 @@
-APP_PROJECT_PATH := $(call my-dir)/..
-
-APP_ABI := armeabi-v7a
-APP_STL := gnustl_static
-APP_CFLAGS := -O3 -DNDEBUG -g 
-APP_PLATFORM := android-15
-APP_PIE := false
-
-include jni/Settings.mk
